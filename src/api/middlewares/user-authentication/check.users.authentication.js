fac9d953251cc78708735e3ef7d9696150da77f1
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2018-11-17 22:59:30 +0100

    Update check.users.authentication.js

--- src/api/middlewares/user-authentication/check.users.authentication.js
+++ src/api/middlewares/user-authentication/check.users.authentication.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) => {
   try {
+    console.log(req.headers);
     const token = req.headers.authorization.split(" ")[1];
     console.log(token);
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
