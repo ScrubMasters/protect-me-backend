0970ec30557b1a961a3c495c0de0dec9423be15e
Korakk <oscarlopezluna@gmail.com>
2018-11-17 15:13:49 +0100

    deploy update

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use((req, res, next) => {
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
       return res.status(200).json({});
   }
+
   next();
+  
 });
 
 app.use("/users/", userRoutes);
