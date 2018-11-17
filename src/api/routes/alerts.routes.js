a317e645ee0f59fc9bd4aa597e787606ae1ff303
Korakk <oscarlopezluna@gmail.com>
2018-11-17 22:18:41 +0100

    Auth added to alerts

--- src/api/routes/alerts.routes.js
+++ src/api/routes/alerts.routes.js
@@ -5,18 +5,18 @@ const checkAuth = require("../middlewares/user-authentication/check.users.authen
 const AlertController = require("../controllers/alerts.controller.js");
 
 //get all alerts
-router.get("/", AlertController.get_all_alerts);
+router.get("/", checkAuth, AlertController.get_all_alerts);
 
 //get alert
-router.get("/:alertId", AlertController.get_alert);
+router.get("/:alertId", checkAuth, AlertController.get_alert);
 
 //create alert with audio
-router.post("/audio", AlertController.create_alert_audio);
+router.post("/audio", checkAuth, AlertController.create_alert_audio);
 
 //create alert without audio
-router.post("/", AlertController.create_alert_no_audio);
+router.post("/", checkAuth, AlertController.create_alert_no_audio);
 
 //delete alert
-router.delete("/:alertId", AlertController.delete_alert);
+router.delete("/:alertId", checkAuth, AlertController.delete_alert);
 
 module.exports = router;
